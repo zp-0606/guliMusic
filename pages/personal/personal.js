c7577de10941de83cd47584a99f131d485b2977f
zp-0606 <1123929807@qq.com>
2022-04-29 18:10:43 +0800

    个人中心静态页面搭建完成

---
+++ pages/personal/personal.js
@@ -0,0 +1,94 @@
+// pages/personal/personal.js
+let startY=0
+let moveY=0
+let moveDistance=0
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    coverTransForm:'translateY(0)',
+    coverTransition:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  handleTouchStart(event) {
+    this.setData({
+      coverTransition:''
+    })
+    startY=event.touches[0].clientY
+  },
+  handleTouchMove(event) {
+     moveY=event.touches[0].clientY
+    moveDistance=moveY-startY
+    if(moveDistance<=0){
+      return
+    }
+    if(moveDistance>=80){
+      moveDistance=80
+    }
+    this.setData({
+      coverTransForm:`translateY(${moveDistance}rpx)`
+    })
+  },
+  handleTouchEnd() {
+    this.setData({
+      coverTransForm:'translateY(0)',
+      coverTransition:'transform 1s linear'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
