b58113b447dff3bcc005faeff79c9d1905e26c68
zp-0606 <1123929807@qq.com>
2022-05-01 18:02:24 +0800

    recommend内容区动态显示

--- pages/recommendSong/recommendSong.js
+++ pages/recommendSong/recommendSong.js
@@ -1,4 +1,5 @@
 // pages/recommendSong/recommendSong.js
+import request from "../../utils/request";
 Page({
 
   /**
@@ -6,16 +7,38 @@ Page({
    */
   data: {
     day:'',
-    month:''
+    month:'',
+    recommendList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let userInfo=wx.getStorageSync('userInfo')
+    if(!userInfo){
+      wx.showToast({
+        title:'请先登录',
+        icon:'none',
+        success:()=>{
+          wx.reLaunch({
+            url:'/pages/login/login'
+          })
+        }
+      })
+    }
     this.setData({
       day:new Date().getDate(),
       month:new Date().getMonth()+1
+    })
+    this.getRecommendSong()
+  },
+
+  //获取每日推荐数据
+  async getRecommendSong(){
+    let recommendSongData=await request('/recommend/songs')
+    this.setData({
+      recommendList:recommendSongData.recommend
     })
   },
 
