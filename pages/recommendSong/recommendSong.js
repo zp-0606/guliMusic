d9b82d183842fa9b41664db4a5f612e65cdd6a88
zp-0606 <1123929807@qq.com>
2022-05-01 16:41:48 +0800

    recommendSong日期动态显示

--- pages/recommendSong/recommendSong.js
+++ pages/recommendSong/recommendSong.js
@@ -5,14 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    day:'',
+    month:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      day:new Date().getDate(),
+      month:new Date().getMonth()+1
+    })
   },
 
   /**
