d6fc779b142a58ba485bce310cbd59247e925d4a
zp-0606 <1123929807@qq.com>
2022-05-04 13:57:49 +0800

    热搜榜动态显示

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,20 +1,30 @@
 // pages/search/search.js
+import request from "../../utils/request";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    keywords:'',
+    hotList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.searchInit()
   },
-
+  //初始化数据
+  async searchInit(){
+    let wordsData=await request('/search/default')
+    let hotListData=await request('/search/hot/detail')
+    this.setData({
+      keywords:wordsData.data.showKeyword,
+      hotList:hotListData.data
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
